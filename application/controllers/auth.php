876415c2faced6c256e0b9f7887f87dda14008b8
tsbasalsabilla-droid <tsbasalsabilla@gmail.com>
2026-03-30 14:23:29 +0700

    update login register

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -13,6 +13,9 @@ public function __construct()
     {   
         parent::__construct();
         $this->load->library('form_validation');
+        $this->load->library('session');
+        $this->load->helper('url');
+        $this->load->database();
         
     }
 
@@ -26,7 +29,7 @@ public function index()
         $this->form_validation->set_rules('password', 'Password', 'required|trim');
 
         if ($this->form_validation->run() == false) { 
-        $data = ['title' => 'login page'];
+        $data = ['title' => 'Login'];
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
@@ -41,7 +44,7 @@ public function index()
         $email =$this->input->post('email');
         $password = $this->input->post('password'); 
 
-        $user = $this->db->get_where('userr', ['email' => $email])->row_array();
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
         
         if($user) {
             //usernya aktif 
@@ -85,7 +88,7 @@ public function index()
         }
 
         $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[userr.email]', [
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
             'is_unique' => 'this email has already registered!'
         ]);
         $this->form_validation->set_rules('password1', 'password', 'required|trim|min_length[3]|matches[password2]', [
@@ -95,22 +98,20 @@ public function index()
         $this->form_validation->set_rules('password2', 'password', 'required|trim|matches[password1]');
 
         if ( $this->form_validation->run() == false) {
-            $data['title'] = 'wpu-registration';
+            $data['title'] = 'Register';
             $this->load->view('templates/auth_header', $data);
-            $this->load->view('auth/registration');
+            $this->load->view('auth/register');
             $this->load->view('templates/auth_footer');
         } else {
             $data = [
                 'name' => htmlspecialchars($this->input->post('name', true)),
                 'email' => htmlspecialchars($this->input->post('email', true)),
-                'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 1,
-                'date' => time()
+                'is_active' => 1
             ];
 
-           $this->db->insert('userr', $data);
+           $this->db->insert('user', $data);
            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please login.</div>');
             redirect('auth');
         }
